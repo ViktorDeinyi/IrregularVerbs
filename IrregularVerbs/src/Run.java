a44485a6ffbd55f062f86af60d35cf8bf7f083e3
Viktor Deinyi <deinyiviktor@gmail.com>
2022-09-07 17:10:10 +0300

    Add Run class and method exit

---
+++ IrregularVerbs/src/Run.java
@@ -0,0 +1,37 @@
+
+
+import java.time.LocalTime;
+import java.util.*;
+
+public class Run {
+    public static final String UKR = "(Ukrainian)";
+    public static final String V1 = "(V1 Infinitive)";
+    public static final String V2 = "(V2 Past Simple)";
+    public static final String V3 = "(V3 Participle)";
+    public static String answer1;
+    public static String answer2;
+    public static String answer3;
+    public static String question;
+
+    public static Map<Integer, String> names = Map.of(
+            0, "(V1 Infinitive)",
+            1, "(V2 Past Simple)",
+            2, "(V3 Participle)",
+            3, "(Ukrainian)");
+
+    public static final String TEXT_RESET = "\u001B[0m";
+    public static final String TEXT_GREEN = "\u001B[32m";
+    public static final String TEXT_RED = "\u001B[31m";
+    public static final String TEXT_YELLOW = "\u001B[33m";
+
+
+    int[] checkOriginal;
+    public static boolean checkNewTest = true;
+
+
+
+    public static void exit() {
+        System.out.println("Thank you for using our application.");
+        System.exit(0);
+    }
+}
